a74398488dd2ee0eeb9084dc9be74eceea1d11d8
Simran-ubhi <gurpreet.clubjb@gmail.com>
2022-08-18 15:37:49 +0530

    sdfd

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -18,13 +18,10 @@ class AdminAuthController extends Controller
     }
 
     public function create(Request $request){
-        
-        
 
-        //validate
+
 
         $request->validate([
-<<<<<<< HEAD
             'Name' => 'required',
             'Phone_Number' => 'required|integer',
             'Email' => 'required|email',
@@ -33,29 +30,6 @@ class AdminAuthController extends Controller
             'Address'=>'required'
         ]);
         $create = Admin::create($request->all());
-=======
-            'admtype' => 'required',
-            'admname' => 'required',
-            'admcontact' => 'required|integer',
-            'admemail' => 'required|email',
-            'adminPassword' =>'required',
-            'adm-DOB' =>'required',
-            'admadrss'=>'required'
-        ]);
-
-        //insert
-        $admin = new Admin;
-        $admin->Category = $request->Category;
-        $admin->admname = $request->admname;
-        $admin->Email = $request->Email;
-        $admin->Phone_Number = $request->Phone_Number;
-        $admin->Password = $request->Password;
-        $admin->D_O_B = $request->D_O_B;
-        $admin->Address = $request->Address;
-
-        $create = $admin->create();
-
->>>>>>> 9df8bc78ba48bdde235a54812caf790ac94f31cf
         if($create){
             return back()->with('Success','New admin created');
         } else {
